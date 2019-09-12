031ea467e03f3575a477a1b995fe283d4d8c3b5a
Alexander Boozer <alexander.boozer.17@cnu.edu>
2019-09-12 11:25:09 -0400

    testing commit and push

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
+#include <string.h>
+#include "myfunc.h"
 
-std::string func()
-{
-	return "hello";
-}
+
 
 int main()
 {
+	std::string func();
+	const int SUCCESS = 0;
 	std::cout<<func()<<std::endl;
-	return 0;
+	return SUCCESS;
 }
+
